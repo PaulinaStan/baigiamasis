964dcfdef6a91d8df160caeb8af8e50cc42318ff
Paulina <paula232@inbox.lt>
2023-03-01 09:57:07 +0200

    pirmasis komitas

---
+++ baigiamasis/app/Http/Controllers/HomeController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Contracts\View\Factory;
+use Illuminate\Contracts\View\View;
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     *
+     * @param Request $request
+     * @return Application|Factory|View
+     */
+    public function __invoke(Request $request)
+    {
+        $products = Product::latest()->paginate(4);
+        return view('home', ['products' => $products]);
+    }
+}
---
+++ baigiamasis/app/Http/Controllers/ProductsController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+
+class ProductsController extends Controller
+{
+public function show(Product $product)
+{
+return view('product_show', ['product' => $product]);
+}
+
+//funkciją, kuri pridėtų produktą į pirkinių krepšelį:
+public function store(Request $request)
+{
+$product = Product::findOrFail($request->product_id);
+Cart::add($product->id, $product->name, 1, $product->price);
+return redirect()->route('cart.index')->with('success_message', 'Item was added to your cart!');
+}
+
+}
